15f46b6b3e6d12b81df5d587f96c143a61623cc8
tw1997 <t888y@qq.com>
2015-11-16 07:31:44 +0800

    tw

--- Application/Runtime/Cache/Home/2b544ac36091689ef184691ed2ea2392.php
+++ Application/Runtime/Cache/Home/2b544ac36091689ef184691ed2ea2392.php
@@ -3,11 +3,13 @@
 <!--header开始-->
 <header class="header-top bs2">
     <a href="#" class="hidden-text" title="后退">后退</a>
-    <h1>登陆维修记录</h1>
+    <h1>选择班级</h1>
 </header>
 <section class="secelt-body">
-    <label class="label">计算机学院/15网络工程2班</label>
-    <ul id="classInfoBody"><?php echo W('SetClass/classesInfo',$classInfoWidgetparam);?></ul>
+    <label class="label" id="currClassInfo"><!----计算机学院/15网络工程2班---></label>
+    <ul id="classInfoBody">
+        <?php echo W('SetClass/classesInfo',$classInfoWidgetparam);?>
+    </ul>
 </section>
     </body>
     <script type="text/javascript" src="<?php echo ($APP_PUBLIC_URL); ?>js/jquery-2.1.4.min.js"></script>
@@ -15,9 +17,32 @@
 </html>
 
 <script type="text/javascript">
-    $('#classInfoBody>li').click(function(){
-        ajaxRequest($(this).attr('data-href'),function(data){
-            alert(data);
-        });
+    //班级信息的UL
+    var JclassInfoBody = $('#classInfoBody');
+    //ajax返回班级信息
+    var classInfo;
+    //当前班级
+    var JcurrClassInfo = $('#currClassInfo');
+    //当前Li
+    var JcurrLi;
+    $(function(){
+        clickItem();
     });
+    function clickItem(){
+        $('#classInfoBody>li').click(function(){
+            JcurrLi = $(this);
+            ajaxRequest(JcurrLi.attr('data-href'),function(data){
+                classInfo = data;
+                //替换内容
+                JclassInfoBody.fadeOut('fast',function(){
+                    JcurrClassInfo.html(JcurrClassInfo.html() + ' ' + JcurrLi.children('p').html());
+                    JclassInfoBody.children().remove();
+                    JclassInfoBody.fadeIn('fast');
+                    JclassInfoBody.append(classInfo);
+                    //再次绑定点击时间
+                    clickItem();
+                });
+            });
+        });
+    }
 </script>
