ea79db33be8d523faa871bfa414c59e9d5035d60
ty666 <1255803549@qq.com>
2015-11-17 19:41:28 +0800

    解决冲突

--- Application/Common/Model/CommentModel.class.php
+++ Application/Common/Model/CommentModel.class.php
@@ -14,19 +14,22 @@ use Think\Model;
 class CommentModel extends Model
 {
     protected $tableName = 'comment';
-    /**
-    id
-    record_id
-    to_replay_id
-    to_uid
-    content
+    /**添加一条评论
+     * @param $data = array(
+     *      record_id=>, //如果是一条评价则有该值
+     *      to_replay_id=>,//如果是回复评论 则有该值
+     *      to_uid=>,//如果是回复评论或者是评价 则有该值
+     *      content=>,//评论内容
+     * );
+     * @return 返回添加的评论id
      */
     public function addComment($data){
         if(!$GLOBALS['e8']['mid']) E('请先登录!');
         $data['uid'] = $GLOBALS['e8']['mid'];
         $data['ctime'] = time();
         if(empty($data['content'])) E('请输入评论内容!');
-        $data['content'] = html2Text($data['content']);
+        $data['content'] = html2Text($data['content']);//安全处理
+
         if(!$data['record_id']){
             //这是一条评价
             //通知维修者收到评价
@@ -38,9 +41,11 @@ class CommentModel extends Model
             //这是对e8的留言
             if(!empty($data['to_replay_id'])){
                 //这是回复评论
-                D('Notify')->sendNotify();
+                //通知被回复者收到回复消息
+                D('Notify')->sendNotify($data['to_uid'],'',);
             }
         }
-        $this->add();
+        echo '------------添加评论的数据-------------<br/>';p($data);echo '------------添加评论的数据-------------<br/>';die;
+        return $this->add($data);
     }
 }
