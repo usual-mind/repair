fb2514f31ccd19caf774bd3bb87e73db11b76615
ty666 <1255803549@qq.com>
2015-11-22 10:51:54 +0800

    ty

--- index.php
+++ index.php
@@ -17,9 +17,13 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 //根目录
-define('ROOT_PATH',str_replace('\\','/',dirname(__FILE__)).'/');
+//绝对路径
+//define('ROOT_PATH',str_replace('\\','/',dirname(__FILE__)).'/');
 // 定义应用目录
-define('APP_PATH',ROOT_PATH.'Application/');
+define('APP_PATH','./Application/');
+//data文件夹目录
+define('USER_DATA_PATH','./data');
+define('USER_UPLOAD_PATH',USER_DATA_PATH.'/uploads');
 header('Content-type:text/html;charset=utf-8');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
